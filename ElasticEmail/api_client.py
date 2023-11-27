786cb118b4612201d8b67a22d440eeabccdf09a4
ElasticEmail <integrations@elasticemail.com>
2023-11-27 14:11:58 +0000

    update pypi package

--- ElasticEmail/api_client.py
+++ ElasticEmail/api_client.py
@@ -1004,7 +1004,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/4.0.22/python'
+        self.user_agent = 'OpenAPI-Generator/4.0.23/python'
 
     def __enter__(self):
         return self
--- ElasticEmail/configuration.py
+++ ElasticEmail/configuration.py
@@ -401,7 +401,7 @@ conf = ElasticEmail.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 4.0.0\n"\
-               "SDK Package Version: 4.0.22".\
+               "SDK Package Version: 4.0.23".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
